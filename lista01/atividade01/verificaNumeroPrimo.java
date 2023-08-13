b81662adbcf2e62229cf4fb64256d987ba22413d
YASMINCOS <costayasmin367@gmail.com>
2023-08-13 18:35:38 -0300

    verificar numero primo

--- lista01/atividade01/verificaNumeroPrimo.java
+++ lista01/atividade01/verificaNumeroPrimo.java
@@ -2,15 +2,33 @@ package lista01.atividade01;
 import java.util.Scanner;
 
 public class verificaNumeroPrimo {
-    public static void main(String args[]){       
-      verificaNumeroPrimo(4);
+    public static void main(String args[]) {       
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int numero = scanner.nextInt();
+        scanner.close();
+        
+        verificaNumeroPrimo(numero);
     }
 
-    private static void verificaNumeroPrimo(Integer n){
-        for (int j = 2; j < n; j++) {
-            if (n % j == 0)
+    private static void verificaNumeroPrimo(int n) {
+        boolean isPrime = true; 
+
+        if (n <= 1) {
+            isPrime = false; // Números menores ou iguais a 1 não são primos
+        } else {
+            for (int j = 2; j < n; j++) {
+                if (n % j == 0) {
+                    isPrime = false;
+                    break; 
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " é primo");
+        } else {
             System.out.println(n + " não é primo");
         }
-        System.out.println(n + " é primo");
     }
 }
