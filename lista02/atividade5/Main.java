4a6d8751a75ec6f3ae17c28e0857e279fd01e4a6
Efraim <efra10_efraim@hotmail.com>
2023-09-28 01:05:08 -0300

    Cadastro do CLIENTE adicionado.

--- lista02/atividade5/Main.java
+++ lista02/atividade5/Main.java
@@ -1,9 +1,34 @@
 package atividade5;
 
+import atividade5.sistemaVendas.controleClientes.Cliente;
+import java.util.Scanner;
+
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Teste");
-        System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        Cliente cliente = new Cliente();
+
+        System.out.println("MERCADINHO\n");
+
+        //Cadastro
+        System.out.println("Cliente");
+
+        //Pessoa >>> Cliente
+        System.out.print("        Nome: ");
+        cliente.setNome(scanner.nextLine());
+        System.out.print("      E-mail: ");
+        cliente.setEmail(scanner.nextLine());
+        System.out.print("         CPF: ");
+        cliente.setCpf(scanner.nextInt());
+        scanner.nextLine();
+        
+        //Cliente
+        System.out.print("    Telefone: ");
+        cliente.setTelefone(scanner.nextLine());
+        System.out.print("    Endereço: ");
+        cliente.setTelefone(scanner.nextLine());
+
+        scanner.close();
     }
 }
